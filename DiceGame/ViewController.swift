3c9de67cf64bd65a24ccd8a2bd72751847f801ec
Kim Ngan <kimngan.ledang@gmail.com>
2021-01-21 22:04:25 +0100

    Random roll

--- DiceGame/ViewController.swift
+++ DiceGame/ViewController.swift
@@ -12,22 +12,19 @@ class ViewController: UIViewController {
     //IBOutlet reference to a UI element
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
-    var leftDiceNumber = 0
-    var rightDiceNumber = 5
+    
+    var dices = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix") ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
-        //change image on first view
-        diceImageView1.image = #imageLiteral(resourceName: "DiceSix") // image literal
-        diceImageView2.image = #imageLiteral(resourceName: "DiceTwo") // image literal
     }
 
     @IBAction func rollButtonPressed(_ sender: UIButton) {
-        diceImageView1.image =  [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")  ] [leftDiceNumber]
-        diceImageView2.image =  [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")  ] [rightDiceNumber]
-        leftDiceNumber = leftDiceNumber + 1
-        rightDiceNumber = rightDiceNumber - 1
-    } 
+
+        diceImageView1.image =  dices[Int.random(in: 0...5)]
+        diceImageView2.image =  dices[Int.random(in: 0...5)]
+      
+    }
 }
 
