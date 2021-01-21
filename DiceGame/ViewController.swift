47b8db468149ecb621f99017418aab5fc5c0e704
Kim Ngan <kimngan.ledang@gmail.com>
2021-01-21 21:20:03 +0100

    add IBOutlet

--- DiceGame/ViewController.swift
+++ DiceGame/ViewController.swift
@@ -8,12 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //IBOutlet reference to a UI element
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        //change image on first view
+        diceImageView1.image = #imageLiteral(resourceName: "DiceSix") // image literal
+        diceImageView2.image = #imageLiteral(resourceName: "DiceTwo") // image literal
     }
-
 
 }
 
