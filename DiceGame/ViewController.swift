4fdd6cc259ef5786c6c7831533cce603327afe58
Kim Ngan <kimngan.ledang@gmail.com>
2021-01-21 21:46:33 +0100

    Roll dice

--- DiceGame/ViewController.swift
+++ DiceGame/ViewController.swift
@@ -12,7 +12,8 @@ class ViewController: UIViewController {
     //IBOutlet reference to a UI element
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
-    
+    var leftDiceNumber = 0
+    var rightDiceNumber = 5
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -23,8 +24,10 @@ class ViewController: UIViewController {
     }
 
     @IBAction func rollButtonPressed(_ sender: UIButton) {
-        diceImageView1.image = #imageLiteral(resourceName: "DiceFour") // image literal
-        diceImageView2.image = #imageLiteral(resourceName: "DiceFour") // image literal
-    }
+        diceImageView1.image =  [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")  ] [leftDiceNumber]
+        diceImageView2.image =  [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")  ] [rightDiceNumber]
+        leftDiceNumber = leftDiceNumber + 1
+        rightDiceNumber = rightDiceNumber - 1
+    } 
 }
 
